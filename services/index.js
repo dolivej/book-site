1c72139c4d217059c3da4eca140717b62accc643
dolivej <davidolivejr@gmail.com>
2023-03-06 19:45:04 -0500

    test

--- services/index.js
+++ services/index.js
@@ -3,6 +3,8 @@ import { request, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
+console.log(graphqlAPI)
+
 export const getAllBooksOverview = async () => {
     const query = gql`
     query Assets {
