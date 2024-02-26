ccf6cfdf88604ca389feda99b52c3f18e70ec9dc
David Olive <50810497+dolivej@users.noreply.github.com>
2024-02-25 22:52:56 -0800

    forcing dynamic behavior

--- pages/api/comments.jsx
+++ pages/api/comments.jsx
@@ -3,6 +3,9 @@ import { getChapter } from '../../services'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
+//forcing dynamic page behavior to get new data from database
+export const dynamic = 'force-dynamic';
+
 export default async function comments(req,res) {
   try {
     const inputData = JSON.parse(req.body)
