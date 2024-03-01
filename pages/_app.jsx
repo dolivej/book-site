540f38fdb5cb9c8fe0938b910d80f8d700d8a3d8
David Olive <50810497+dolivej@users.noreply.github.com>
2024-02-29 22:30:32 -0800

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
 import Script from 'next/script';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <>
